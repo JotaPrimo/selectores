5ac54187ba9bca2f7739d542612fcdd4de9d165e
Jota Santos <jailsonsantosdpge@outlook.com>
2024-10-02 15:31:37 -0400

    267. Selector de regiones

--- src/app/country/countries/pages/selector-page/selector-page.component.ts
+++ src/app/country/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../../../services/countries.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Region } from '../../../interfaces/country.interfaces';
 
 @Component({
   selector: 'countries-selector-page',
@@ -16,12 +18,17 @@ export class SelectorPageComponent {
   });
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private countriesService: CountriesService
   ) { }
 
 
   handleSubmit() {
     console.log("submit");
+  }
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
   }
 
 }
--- src/app/country/services/countries.service.ts
+++ src/app/country/services/countries.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Region } from '../interfaces/country.interfaces';
 
 
 // esse root indica que a mesma instancia estará a disposição
@@ -8,6 +9,21 @@ import { Injectable } from '@angular/core';
 })
 export class CountriesService {
 
+  private _regions: Region[] = [
+    Region.Africa,
+    Region.Americas,
+    Region.Asia,
+    Region.Europe,
+    Region.Oceania
+  ];
+
 constructor() { }
+
+// o spread rompe as reações que existem com as regioes
+// significa que se for alterado esse valor, o array original está protegido
+// dessa alteração
+get regions() {
+  return [...this._regions];
+}
 
 }
