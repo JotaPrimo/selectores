fae9fafd96a3c020fd6184a01f8099834ef80c2d
Jota Santos <jailsonsantosdpge@outlook.com>
2024-10-02 14:48:32 -0400

    266. Servicio e interfaces

---
+++ src/app/country/services/countries.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+
+// esse root indica que a mesma instancia estará a disposição
+// para toda a aplicação
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+constructor() { }
+
+}
